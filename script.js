49b162b708949858234480acd088b92d2851ce46
Kawayan <kawayan.deguzman@gmail.com>
2022-09-28 23:43:28 -0400

    add javascript password confirmation

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-const element = document.getElementsByClassName("btn");
-element.addEventListener("click", myFunction);
+var password = document.getElementById("password");
+var confirm_password = document.getElementById("confirm-password");
 
-
-
-function myFunction() {
-    if (element.classList.contains("active"));{
-        $().button('toggle')
-    }
-    }
-
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict';
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach((form) => {
-      form.addEventListener('submit', (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  })();
-
-  $(document).ready(function() {
-    $("img").on("contextmenu",function(){
-      return false;
-    }); 
-});
+if (password == confirm_password) {
+  document.getElementById("password").style.border = "1.5px solid green";
+}
