77769be4ca9bcb47abc5868c39a8890f68c65225
Kawayan <kawayan.deguzman@gmail.com>
2022-09-30 23:42:07 -0500

    add comments for js password check

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 var password = document.getElementById("password");
 var confirm_password = document.getElementById("confirm-password");
 
+
+//compare password to see if same 
 if (password == confirm_password) {
   document.getElementById("password").style.border = "1.5px solid green";
 }
