77633db76c69c055ec132e268ed4c55c2974c0cf
Kawayan <kawayan.deguzman@gmail.com>
2022-10-03 22:34:41 -0700

    create password match alert

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
-//select password and store it in variable
-var password = document.getElementById("password");
-var confirm_password = document.getElementById("confirm-password");
+// //select password and store it in variable
+// var password = document.getElementById("password");
+// var confirm_password = document.getElementById("confirm-password");
 
 
-//compare password to see if same 
-if (password == confirm_password) {
-  document.getElementById("password").style.border = "1.5px solid green";
-}
+// //compare password to see if same 
+// if (password == confirm_password) {
+//   document.getElementById("password").style.border = "1.5px solid green";
+// }
+
+const create = document.querySelector('#password');
+
+create.addEventListener('click', () => {
+    let pass = document.querySelector('#password').value;
+    let passConfirm = document.querySelector('#confirm-password').value;
+
+    if (passConfirm !== pass) {
+        alert('Password fields must match.');
+    }
+});
